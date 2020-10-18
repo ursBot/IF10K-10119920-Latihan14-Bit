fa64b5ec37637a3bf8a122e47e6beb02f5c9cd3a
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-18 17:22:12 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan14/bit/Bit.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan14.bit;
+
+
+/**
+*
+ * @author
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class Bit{
+    
+    public static void main(String[] args) {
+        int a = 60;/* 60 = 0011 1100 */
+        int b = 13;/* 13 = 0000 1101 */
+        int c = 0;
+        c = a & b;
+        /* 12 = 000 1100 */
+        System.out.println("a & b = " + c);
+        c = a | b;
+        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c);
+        c = ~a;
+        /*-61 = 1100 0011 */
+        System.out.println("~a = " + c);
+        c = a << 2;
+        /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c);
+        c = a >> 2;
+        /* 15 = 0000 1111 */
+        System.out.println("a >> 2 = " + c);
+    }
+
+}
